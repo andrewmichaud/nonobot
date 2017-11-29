0c73552487ef585fb605e8cff57564bd86cc4c36
Andrew Michaud <dev@mail.andrewmichaud.com>
2017-11-28 21:23:40 -0800

    working seamcarving

--- nonogen/outline.py
+++ nonogen/outline.py
@@ -1,4 +1,6 @@
+import collections
 import itertools
+import random
 import sys
 from copy import deepcopy
 from os import path
@@ -21,13 +23,82 @@ class SeamCarveData:
         self.energy = 0
         self.x = 0
         self.y = 0
+        self.rel_x = 0
+        self.parent_choices = []
         self.parent = None
+        self.children = []
         self.pixel = None
         self.marked = False
 
-
     def __str__(self):
         return str(self.__dict__)
+
+    def __lt__(self, other):
+        return self.energy < other.energy
+
+    def __le__(self, other):
+        return self.energy <= other.energy
+
+    def __gt__(self, other):
+        return self.energy > other.energy
+
+    def __ge__(self, other):
+        return self.energy >= other.energy
+
+    def __eq__(self, other):
+        return self.energy == other.energy
+
+    def __ne__(self, other):
+        return self.energy != other.energy
+
+    def fix_rel_x(self):
+        """Fix rel x."""
+        self.rel_x -= 1
+
+        return self
+
+    def choose_parent(self):
+        """Choose parent and update my energy."""
+        self.energy = get_energy(self.pixel)
+
+        self.parent = random.choice(self.parent_choices)
+        min_energy = self.parent.energy
+        for choice in self.parent_choices:
+            if choice.energy < min_energy:
+                min_energy = choice.energy
+                self.parent = choice
+
+        if min_energy < sys.maxsize - self.energy:
+            self.energy = min_energy + self.energy
+        else:
+            self.energy = sys.maxsize
+
+    def choose_children(self, energies):
+        """Pick children for updating later if we get chosen as part of a seam."""
+
+        # No children in the last row.
+        if self.y == len(energies) - 1:
+            return
+
+        # Parents only need to choose children once.
+        if len(self.children) > 0:
+            return
+
+        # Ensure we always have three children, to make later logic simpler.
+        # Left.
+        if self.x > 0:
+            self.children.append(energies[self.y+1][self.x-1])
+        else:
+            self.children.append(None)
+
+        # Middle
+        self.children.append(energies[self.y+1][self.x])
+
+        # Right.
+        if self.x < len(energies[0]) - 1:
+            self.children.append(energies[self.y+1][self.x+1])
+        else:
+            self.children.append(None)
 
 class OutlineData:
     """Data for outlining."""
@@ -90,7 +161,7 @@ def get_neighbors(pixels, x, y, height, width):
     # Diagonals?
     return neighbors
 
-def horiz_seamcarve(in_name=IN_NAME, count=100, show=True):
+def horiz_seamcarve(in_name=IN_NAME, count=10, show=True):
     """Seamcarve image N times."""
     im = Image.open(in_name)
     im_pixels = im.load()
@@ -104,84 +175,80 @@ def horiz_seamcarve(in_name=IN_NAME, count=100, show=True):
 
         out_pixels.append(row)
 
+    energies = calculate_energies(im_pixels, im.height, im.width)
+
+    # Get sorted list of bottom row to get our seams.
+    seam_starts = sorted(random.sample(energies[-1], len(energies[-1])))
+
     # Carve!
+    count = min(count, im.width)
     for i in range(count):
-        if i == 0:
-            base_energies = create_base_energy(im_pixels, im.height, im.width)
-            energies = calculate_energies(im_pixels, base_energies, im.height, im.width)
-        else:
-            energies = calculate_energies(im_pixels, energies, im.height, im.width)
 
-        # Find start point.
-        y = im.height-1
-        min_sc_data = energies[y][0]
-        for x in range(1, im.width, 1):
-            if not energies[y][x].marked and energies[y][x].energy < min_sc_data.energy:
-                min_sc_data = energies[y][x]
+        min_sc_data = seam_starts[i]
 
-        sc_data = min_sc_data
-        while sc_data is not None:
-            # Mark if show is set to true, otherwise delete.
+        # Get top of seam.
+        seam = collections.deque([min_sc_data])
+        parent = min_sc_data.parent
+        while parent is not None:
+            seam.appendleft(parent)
+            parent = parent.parent
+
+        for s, elem in enumerate(seam):
+
+            # Update out for each pixel in seam.
             if show:
-                out_pixels[sc_data.y][sc_data.x] = (255, 0, 0, 254)
+                out_pixels[elem.y][elem.x] = (255, 0, 0, 254)
             else:
-                out_pixels[sc_data.y].pop(sc_data.x-i)
+                out_pixels[elem.y] = out_pixels[elem.y][:elem.rel_x] + \
+                        out_pixels[elem.y][elem.rel_x+1:]
 
-            sc_data.marked = True
+            # Remove pixel from energies.
+            energies[elem.y] = energies[elem.y][:elem.rel_x] +\
+                    list(map(lambda x: x.fix_rel_x(), energies[elem.y][elem.rel_x+1:]))
 
-            sc_data = sc_data.parent
+            if s < len(seam) - 1:
+                # Have each child of pixel update.
+                # They can't choose this pixel in the seam anymore.
+                # TODO this is gonna be annoying to generalize.
+
+                left_child = elem.children[0]
+                middle_child = elem.children[1]
+                right_child = elem.children[2]
+
+                # Handle left child.
+                if left_child is not None:
+                    left_child.parent_choices = left_child.parent_choices[0:2]
+
+                    # Watch right edge of image (moving target because of deletions).
+                    if elem.rel_x < len(energies[elem.y]):
+                        left_child.parent_choices.append(energies[elem.y][elem.rel_x])
+
+                    left_child.choose_parent()
+
+                # Handle middle child.
+                middle_child.parent_choices = middle_child.parent_choices[0:1] +\
+                        middle_child.parent_choices[2:3]
+
+                # Watch right edge of image (moving target because of deletions).
+                if elem.rel_x < len(energies[elem.y]):
+                    middle_child.parent_choices.append(energies[elem.y][elem.rel_x])
+
+                middle_child.choose_parent()
+
+                # Handle right child.
+                right_child.parent_choices = right_child.parent_choices[1:3]
+
+                # Watch left edge of image (moving target because of deletions).
+                if elem.rel_x > 0:
+                    right_child.parent_choices = [energies[elem.y][elem.rel_x-1]] +\
+                        right_child.parent_choices
+
+                right_child.choose_parent()
 
     return out_pixels
 
-def calculate_energies(im_pixels, energies, height, width):
-    """(Re)calculate grid of energies for image."""
-
-    # Do the rest.
-    for y in range(1, height):
-        for x in range(width):
-            sc_data = energies[y][x]
-            sc_data.x = x
-            sc_data.y = y
-            sc_data.pixel = im_pixels[(x, y)]
-
-            here_energy = get_energy(im_pixels[(x, y)])
-
-            if x > 0 and not energies[y-1][x-1].marked:
-                ul_between = get_energy_between(sc_data, energies[y-1][x-1])
-            else:
-                ul_between = sys.maxsize
-
-            if energies[y-1][x].marked:
-                up_between = sys.maxsize
-            else:
-                up_between = get_energy_between(sc_data, energies[y-1][x])
-
-            if x < width - 1 and not energies[y-1][x+1].marked:
-                ur_between = get_energy_between(sc_data, energies[y-1][x+1])
-            else:
-                ur_between = sys.maxsize
-
-            min_of = min(ul_between, up_between, ur_between)
-            if min_of == sys.maxsize:
-                sc_data.energy = min_of
-            else:
-                sc_data.energy = here_energy + min_of
-
-            if ul_between == min_of:
-                sc_data.parent = energies[y-1][x-1]
-
-            elif up_between == min_of:
-                sc_data.parent = energies[y-1][x]
-
-            else:
-                sc_data.parent = energies[y-1][x+1]
-
-            energies[y][x] = sc_data
-
-    return energies
-
-def create_base_energy(im_pixels, height, width):
-    """Create base energies array."""
+def calculate_energies(im_pixels, height, width):
+    """Calculate grid of energies for image."""
     energies = [[SeamCarveData() for i in range(width)] for r in range(height)]
 
     # Do first row.
@@ -189,9 +256,39 @@ def create_base_energy(im_pixels, height, width):
         sc_data = energies[0][x]
         sc_data.x = x
         sc_data.y = 0
+        sc_data.rel_x = x
         sc_data.pixel = im_pixels[(x, 0)]
         sc_data.energy = get_energy(im_pixels[(x, 0)])
-        energies[0][x] = sc_data
+
+    # Do the rest.
+    for y in range(1, height):
+        for x in range(width):
+
+            # Set up sc_data.
+            sc_data = energies[y][x]
+            sc_data.x = x
+            sc_data.y = y
+            sc_data.rel_x = x
+            sc_data.pixel = im_pixels[(x, y)]
+
+            # Choose parent options, minding edges.
+            # Left.
+            if x > 0:
+                sc_data.parent_choices.append(energies[y-1][x-1])
+
+            # Middle.
+            sc_data.parent_choices.append(energies[y-1][x])
+
+            # Right.
+            if x < width - 1:
+                sc_data.parent_choices.append(energies[y-1][x+1])
+
+            # Have parents choose children.
+            for parent in sc_data.parent_choices:
+                parent.choose_children(energies)
+
+            # Choose cheapest parent.
+            sc_data.choose_parent()
 
     return energies
 
